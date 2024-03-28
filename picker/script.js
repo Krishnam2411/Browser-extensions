f5e0f6a6a6f58594bc41f65c7b2312a87215afbd
Krishnam Maheshwari <124492864+Krishnam2411@users.noreply.github.com>
2024-03-28 14:18:18 +0530

    "FIX: error handling"

--- picker/script.js
+++ picker/script.js
@@ -1,17 +1,20 @@
-const body = document.querySelector('body')
-const button = document.getElementById('button')
-const color = document.getElementById('color')
-const lastColor = localStorage.getItem('picker-color')
+const body = document.querySelector("body");
+const button = document.getElementById("button");
+const color = document.getElementById("color");
+const lastColor = localStorage.getItem("picker-color");
 
-button.style.backgroundColor = lastColor
-color.textContent = lastColor
-button.addEventListener('click', async () => {
-    body.style.display = "none"
-    const eyedropper = new EyeDropper()
-    const { sRGBHex } = await eyedropper.open()
-    button.style.backgroundColor = sRGBHex
-    color.textContent = sRGBHex.toUpperCase()
-    body.style.display = "block"
-    await navigator.clipboard.writeText(sRGBHex.toUpperCase())
-    localStorage.setItem('picker-color', sRGBHex.toUpperCase())
-})
+if (lastColor) {
+  button.style.backgroundColor = lastColor;
+  color.textContent = lastColor;
+}
+
+button.addEventListener("click", async () => {
+  body.style.display = "none";
+  const eyedropper = new EyeDropper();
+  const { sRGBHex } = await eyedropper.open();
+  button.style.backgroundColor = sRGBHex;
+  color.textContent = sRGBHex.toUpperCase();
+  body.style.display = "block";
+  await navigator.clipboard.writeText(sRGBHex.toUpperCase());
+  localStorage.setItem("picker-color", sRGBHex.toUpperCase());
+});
